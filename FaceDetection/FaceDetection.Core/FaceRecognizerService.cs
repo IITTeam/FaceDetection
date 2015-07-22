f4ba9216230c5c11bd5417b4b4dd7bf601d1d7d1
Yusupov Danil <yusupdan@gmail.com>
2015-07-22 12:28:19 +0500

    FaceRecognizedService

--- FaceDetection/FaceDetection.Core/FaceRecognizerService.cs
+++ FaceDetection/FaceDetection.Core/FaceRecognizerService.cs
@@ -1,17 +1,41 @@
-﻿using Emgu.CV;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
 
 namespace FaceDetection.Core
 {
     public class FaceRecognizerService
     {
-        /// <summary>
-        /// Делает фото с веб-камеры
-        /// </summary>
+        FaceRecognizer faceRecognizer;
+
+        public FaceRecognizerService()
+        {
+            faceRecognizer = new FisherFaceRecognizer();
+        }
+
         public void StartCapture()
         {
-            var capture = new Capture(); //create a camera captue
-            var photo = capture.QueryFrame(); //draw the image obtained from camera
-            photo.Save("test.jpg");
+
+        }
+
+        public void Train(Dictionary<int, List<Image<Bgr, float>>> trainSamples)
+        {
+            var labels = new List<int>();
+            var images = new List<Image<Bgr, float>>();
+            foreach (var key in trainSamples.Keys)
+            {
+                foreach (var image in trainSamples[key])
+                {
+                    labels.Add(key);
+                    images.Add(image);
+                }
+            }
+            faceRecognizer.Train(images.ToArray(), labels.ToArray());
         }
     }
 }
