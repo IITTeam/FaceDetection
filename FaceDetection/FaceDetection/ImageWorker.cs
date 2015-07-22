48ef640c824a5660bf871de5c78ff940aa0a4123
Mirann <annaibakaeva@google.com>
2015-07-22 12:48:23 +0600

    + класс для получения изображения из файла

---
+++ FaceDetection/FaceDetection/ImageWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV.Face;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceDetection
+{
+    class ImageWorker
+    {
+        public ImageWorker()
+        {
+        }
+
+        public Image<Bgr, float> GetImage()
+        {
+            return null;
+        }
+    }
+}
