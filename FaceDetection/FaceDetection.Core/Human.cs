4d6e4a47ffe052576de9fdd31d04cdd2d8395ca8
Mirann <annaibakaeva@google.com>
2015-07-24 11:29:02 +0600

    change human constructor

--- FaceDetection/FaceDetection.Core/Human.cs
+++ FaceDetection/FaceDetection.Core/Human.cs
@@ -10,11 +10,11 @@ namespace FaceDetection.Core
         public string Name { get; private set; }
         public List<Image<Bgr, byte>> Images { get; private set; }
 
-        public Human(int id, string name)
+        public Human(int id, string name, List<Image<Bgr, byte>> images)
         {
             this.Id = id;
             this.Name = name;
-            this.Images = new List<Image<Bgr, byte>>();
+            this.Images = new List<Image<Bgr, byte>>(images);
         }
     }
 }
