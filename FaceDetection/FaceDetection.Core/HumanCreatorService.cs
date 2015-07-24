fdbf0e64a352ad1ccdab3eb267de48c6633d4749
Mirann <annaibakaeva@google.com>
2015-07-24 11:26:20 +0600

    + human creation

---
+++ FaceDetection/FaceDetection.Core/HumanCreatorService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceDetection.Core
+{
+    class HumanCreatorService
+    {
+        List<Human> People {get; set; }
+
+        public HumanCreatorService()
+        {
+            People = new List<Human>();
+        }
+
+        public void AddHuman(string label, List<Image<Bgr, byte>> images)
+        {
+            People.Add(new Human(People.Count, label, images));
+        }
+
+        public void AddHuman(Human human)
+        {
+            People.Add(human);
+        }
+    }
+}
